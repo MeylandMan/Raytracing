7a1606b49d1fae257c02f41fded810f14a88a6df
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-02-14 23:28:07 +0100

    Setup and image converter

--- App/App.cpp
+++ App/App.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 
-int main()
-{
+#include <iostream>
+#include <fstream>
+
+int main() {
     // Image
     int image_width = 256;
     int image_height = 256;
 
-    // Render
+    // Création du fichier PPM
+    std::ofstream image_file("../image.ppm");
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    // Vérification si le fichier s'est bien ouvert
+    if (!image_file) {
+        std::cerr << "Erreur lors de la création du fichier image.ppm\n";
+        return 1;
+    }
 
-    for (unsigned int j = 0; j < image_width; j++) {
-        for (unsigned int i = 0; i < image_height; i++) {
+    // En-tête PPM
+    image_file << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    // Génération des pixels
+    for (unsigned int j = 0; j < image_height; j++) {  // Correction de l'inversion hauteur/largeur
+        std::clog << "\rScanlines remaining: " << (image_width - j) << " " << std::flush;
+        for (unsigned int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.0;
@@ -20,9 +32,16 @@ int main()
             int ig = int(255.999 * g);
             int ib = int(255.999 * b);
 
-            std::cout << ir << " " << ig << " " << ib << " ";
+            image_file << ir << " " << ig << " " << ib << " ";
         }
-
-        std::cout << "\n";
+        image_file << "\n";
     }
+
+   
+    // Fermeture du fichier
+    image_file.close();
+    std::clog << "\rDone.                  \n";
+
+    return 0;
 }
+
---
+++ generate.bat
@@ -0,0 +1,5 @@
+echo Generating image.png
+magick image.ppm image.png
+echo Generation successful
+
+pause
